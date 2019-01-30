1a41f17ff5dba9aba660a8ac0dd087ceac5d6164
Sebastian <sebastiancamilomarin@gmail.com>
2019-01-30 17:32:34 -0500

    Update app.js
    Corrigiendo

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const express = require('express');
+
+const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
   let name = req.query.nombre;
-  if(!name || name === 0){
+  if (!name || name.length === 0) {
     name = "desconocido";
   }
-  else{
-    res.send(`<h1>Hola ${name}!</h1>`);
 
-  }
-
+  res.send(`<h1>Hola ${name}!</h1>`);
 });
-
-
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
